81fdf6d29747adb2bd039df3ad3f63a662ab4be5
fastigo-ai <fastigo.in@gmail.com>
2025-12-24 14:11:32 +0530

    home1

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import StatusScreen from "./components/onboarding/StatusScreen";
-import Main from "./components/main";
+import Main from "./components/Main";
 const queryClient = new QueryClient();
 
 const App = () => (
